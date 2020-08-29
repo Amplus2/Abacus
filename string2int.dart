54872f56d3ab963f849e7ad9b42a13615ab84c06
Chris (@ Windows) <chrissx@chrissx.de>
2020-08-29 17:32:39 +0200

    finished parsing the bits

--- string2int.dart
+++ string2int.dart
@@ -2,17 +2,25 @@ import 'dart:typed_data';
 
 Uint8List string2int(String s) {
   assert(s.length % 5 == 0);
-  List<bool> bits = [];
+  List<int> bits = [];
   for (var i = 0; i < s.length; i += 5) {
     for (var j = i + 1; j < i + 5; j++) {
       assert(s[j] == '-');
     }
     assert(s[i] == '-' || s[i] == '.');
-    bits.add(s[i] == '.');
+    bits.add(s[i] == '.' ? 1 : 0);
   }
   assert(bits.length % 8 == 0);
   Uint8List bytes;
   for (var i = 0; i < bits.length; i += 8) {
-    bytes.add(value);
+    bytes.add(bits[i + 0] << 7 |
+        bits[i + 1] << 6 |
+        bits[i + 2] << 5 |
+        bits[i + 3] << 4 |
+        bits[i + 4] << 3 |
+        bits[i + 5] << 2 |
+        bits[i + 6] << 1 |
+        bits[i + 7] << 0);
   }
+  return bytes;
 }
