d26bb73068dcfa0bcf1d370d9de299f804d6f47f
Chris (@ Windows) <chrissx@chrissx.de>
2020-09-06 13:12:07 +0200

    cleaned up ludwigs code more

--- lexer.dart
+++ lexer.dart
@@ -1,18 +1,12 @@
 import 'string2int.dart';
 
-//LEXICAL ANALYZER
-class Lexer {
-  Lexer(this.text);
+final _dash = '-'.codeUnitAt(0), _dot = '.'.codeUnitAt(0);
 
-  final String text;
-  static final _dash = '-'.codeUnitAt(0), _dot = '.'.codeUnitAt(0);
-
-  List<int> lex() {
-    var checkText = '';
-    for (var i = 0; i < text.length; i++) {
-      var codeUnit = text.codeUnitAt(i);
-      if (codeUnit == _dash || codeUnit == _dot) checkText += text[i];
-    }
-    return string2int(checkText);
+List<int> lex(final String text) {
+  var checkText = '';
+  for (var i = 0; i < text.length; i++) {
+    var codeUnit = text.codeUnitAt(i);
+    if (codeUnit == _dash || codeUnit == _dot) checkText += text[i];
   }
+  return string2int(checkText);
 }
--- main.dart
+++ main.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
-import 'lexer.dart';
+import 'lexer.dart' as Lexer;
 
 void main(List<String> argv) async {
   Stream<List<int>> input = stdin;
   if (argv.isNotEmpty) input = File(argv.first).openRead();
   var allBytes = <int>[];
   await for (List<int> bytes in input) allBytes.addAll(bytes);
-  print(Lexer(utf8.decode(allBytes)).lex().toList());
+  print(Lexer.lex(utf8.decode(allBytes)).toList());
 }
