1a198b34ea315593d2a076496098ba44a7ee0127
Chris (@ Windows) <chrissx@chrissx.de>
2020-09-10 17:46:50 +0200

    wrote and hand-assembled a hello world program

--- main.dart
+++ main.dart
@@ -3,6 +3,9 @@ import 'string2int.dart';
 import 'riscx.dart' as RISCX;
 
 void main(List<String> argv) async {
-  if (argv.isEmpty) return;
+  if (argv.isEmpty) {
+    print('Usage: dart main.dart [aaff file]');
+    return;
+  }
   RISCX.Machine.run(string2int((await AaffFile.read(argv.first)).decode()));
 }
